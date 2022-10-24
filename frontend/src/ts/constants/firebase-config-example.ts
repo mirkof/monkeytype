d467e4f2531bbd50735aa44be67989ed2c3409cf
Mirko Francuski <mirko.francuski@ing.com>
2022-10-24 16:58:35 +0200

    merge package.json

--- frontend/src/ts/constants/firebase-config-example.ts
+++ frontend/src/ts/constants/firebase-config-example.ts
@@ -3,12 +3,12 @@
 // scroll down to Your apps > Web Apps (if it doesnt exist, create one) > SDK setup and configuration > select npm
 // your config should be visible there
 
-export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDE-lzf5XJK6Y4OAMKSoe-7v-SetJIs4Zo",
+//   authDomain: "monkeytype-b0a63.firebaseapp.com",
+//   databaseURL: "",
+//   projectId: "monkeytype-b0a63",
+//   storageBucket: "monkeytype-b0a63.appspot.com",
+//   messagingSenderId: "41640999710",
+//   appId: "1:41640999710:web:0c49bfbd375f327b67270f"
+// };
